a9c990798e9535d67e8752d7774fd4fd4daff75f
arbust908@gmail.com <Arbust908@gmail.com>
2019-12-10 23:28:53 -0300

    Cosas sin terminar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,38 +2,15 @@
 
 @section('content')
 <div class="flex flex-col">
-    <div class="py-5 px-3 flex items-center justify-center">
-        <div class="flex flex-col justify-around h-full">
-            <div>
-                <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
-                    {{ config('app.name', 'Laravel') }}
-                </h1>
-                <ul class="list-reset">
-                    @foreach (__('routes') as $key => $value)
-                    <li class="inline pr-8">
-                    <a
-                        href="{{ url( '/' . __('routes.'.$key ) ) }}"
-                        class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase"
-                        title="{{ $value }}">
-                        {{ $value }}
-                    </a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    </div>
-    <section class="w-full flex flex-col"> {{-- imagen de fondo --}}
-        <h1 class="text-main-200 text-xxl">@lang('home.title')</h1>
-        <br>
+    <section name="hero">
+        <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
+            @lang('home.title')
+        </h1>
         <p>@lang('home.subtitle')</p>
-        <br>
     </section>
-    <section>
-        @foreach( __('home.cards') as $key => $value )
-            {{ $key . '  ' .$value }}
-            <br>
-        @endforeach
+    <section name="features">
+        {{-- Blob con icono nombre y link --}}
+        @each('paginas.components.home_card', __('home.cards'), 'card')
     </section>
 </div>
 @endsection
---
+++ resources/views/paginas/components/home_card.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ url($card) }}" class="bg-main-200 py-1 px-4 text-white text-base mb-3">
+    <img src="{{ $card }}" alt="Icono de {{$card}}" class="mr-8">
+    <h3>{{ $card }}</h3>
+</a>
