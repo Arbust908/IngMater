5f0946f4500da5fcf5211b94c7718dab3052f272
arbust908@gmail.com <fblanco@digitalhouse.com>
2020-03-22 08:11:55 -0300

    1

--- resources/lang/es/footer.php
+++ resources/lang/es/footer.php
@@ -2,7 +2,7 @@
 <?php
 
 return [
-    "inspiration" => '“Asesorando a empresas acerca de Ingeniería y Ciencia de los materiales”',
+    "inspiration" => '“Asesorando a empresas acerca de Ingeniería y Ciencia de los Materiales”',
     "newsletter" => 'Nuestro Newsletter',
     "subscribe" => 'Quiero suscribirme',
     "placeholder" => 'mimail@mail.com'
